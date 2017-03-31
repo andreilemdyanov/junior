55b3f8deb7e4b7c0da5195749626d9e4b2ff0ca8
andreilemdyanov <lemdyanov5@mail.ru>
2017-03-31 17:56:32 +0400

    task 4087

--- chapter_001/src/main/java/ru/job4j/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/Calculate.java
@@ -1,6 +1,16 @@
 package ru.job4j;
 
+/**
+* Class Calculate решение задачи части 001 урок 1 
+* @author Lemdyanov
+* @since 31.03.2017
+*/
 public class Calculate {
+	
+/**
+* Точка входа в программу
+* метод выводит в консоль "Hello world"
+*/	
 	public static void main(String[] args) {
 		System.out.println("Hello world");
 	}
