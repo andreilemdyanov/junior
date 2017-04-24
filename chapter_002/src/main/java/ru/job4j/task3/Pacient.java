5e027ccf440eafd6ff5c41562465cf99890281d1
andreilemdyanov <lemdyanov5@mail.ru>
2017-04-25 02:09:52 +0400

    task 6837

---
+++ chapter_002/src/main/java/ru/job4j/task3/Pacient.java
@@ -0,0 +1,32 @@
+package ru.job4j.task3;
+/**
+*Class Pacient.
+*
+*@author Andrey Lemdyanov
+*@since 20.04.2017
+*/
+public class Pacient {
+	/**
+	*Имя пациента.
+	*/
+	private String name;
+	/**
+	*Геттер имени.
+	*@return имя.
+	*/
+	public String getName() {
+		return this.name;
+	}
+	/**
+	*Конструкор по умолчанию.
+	*/
+	public Pacient() {
+	}
+	/**
+	*Конструктор.
+	*@param name имя.
+	*/
+	public Pacient(String name) {
+		this.name = name;
+	}
+}
