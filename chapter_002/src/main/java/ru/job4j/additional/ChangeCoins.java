4d8bec132083e3201264edcc5abf5caba6d9b9d9
andreilemdyanov <lemdyanov5@mail.ru>
2017-06-06 23:53:21 +0400

    Additional task

---
+++ chapter_002/src/main/java/ru/job4j/additional/ChangeCoins.java
@@ -0,0 +1,80 @@
+package ru.job4j.additional;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * Class ChangeCoins.
+ *
+ * @author Andrey Lemdyanov {lemdyanov5@mail.ru)
+ * @version $Id$
+ * @since 06.06.2017
+ */
+public class ChangeCoins {
+    /**
+     * Поле сумма для размена.
+     */
+    int cash;
+
+    /**
+     * Конструктор.
+     * @param cash сумма.
+     */
+    public ChangeCoins(int cash) {
+        this.cash = cash;
+    }
+
+    /**
+     * Поле для простого размена.
+     */
+    List<Integer> list = new ArrayList<>();
+    /**
+     * Поле для всех возможных разменов.
+     */
+    List<List<Integer>> list2 = new ArrayList<>();
+
+    /**
+     * Простой размен.
+     * Поле rem - остаток.
+     * ten - количество десяток.
+     * five - количество пятерок.
+     * one - количество единиц.
+     * @return список.
+     */
+    public List<Integer> changeSimple() {
+        int rem, ten, five, one;
+
+        ten = cash / 10;
+        rem = cash % 10;
+        five = rem / 5;
+        rem = rem % 5;
+        one = rem;
+
+
+        list.add(ten);
+        list.add(five);
+        list.add(one);
+        return list;
+    }
+
+    /**
+     * Все возможные размены.
+     * @return список списков.
+     */
+    public List<List<Integer>> changeComplex() {
+        for (int i = 0; i < cash / 10; i++) {
+            for (int j = 0, k = cash - i * 10; k >= 0; j++, k -= 5) {
+                list2.add(new ArrayList<Integer>(Arrays.asList(i, j, k)));
+            }
+        }
+        return list2;
+    }
+
+//    public static void main(String[] args) {
+//        ChangeCoins simple = new ChangeCoins(109);
+//        System.out.println(simple.changeSimple());
+//        ChangeCoins complex = new ChangeCoins(100);
+//        System.out.println(complex.changeComplex());
+//    }
+}
---
+++ chapter_002/src/test/java/ru/job4j/additional/ChangeCoinsTest.java
@@ -0,0 +1,42 @@
+package ru.job4j.additional;
+
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+/**
+ * Class ChangeCoinsTest.
+ *
+ * @author Andrey Lemdyanov {lemdyanov5@mail.ru)
+ * @version $Id$
+ * @since 06.06.2017
+ */
+public class ChangeCoinsTest {
+    /**
+     * Тест простого размена.
+     */
+    @Test
+    public void whenYourCashSimpleChange() {
+        ChangeCoins simple = new ChangeCoins(109);
+        List<Integer> result = simple.changeSimple();
+        List<Integer> expected = new ArrayList<>(Arrays.asList(10, 1, 4));
+        assertThat(result, is(expected));
+    }
+
+    /**
+     * Тест всех разменов.
+     */
+    @Test
+    public void whenYourCashComplexChange() {
+        ChangeCoins complex = new ChangeCoins(100);
+        List<Integer> result = complex.changeComplex().get(8);
+        List<Integer> expected = new ArrayList<>(Arrays.asList(0, 8, 60));
+        assertThat(result, is(expected));
+    }
+
+}
