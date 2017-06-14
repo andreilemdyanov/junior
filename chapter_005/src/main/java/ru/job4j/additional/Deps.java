426f58ae195d6fb0fd308f911495dbe262d908f9
andreilemdyanov <lemdyanov5@mail.ru>
2017-06-15 00:04:51 +0400

    Additional task

--- chapter_005/src/main/java/ru/job4j/additional/Deps.java
+++ chapter_005/src/main/java/ru/job4j/additional/Deps.java
@@ -2,6 +2,7 @@ package ru.job4j.additional;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Comparator;
 import java.util.List;
 
 /**
@@ -20,11 +21,44 @@ public class Deps {
     /**
      * Конструктор.
      */
-    public Deps() {
-        MasSort n = new MasSort();
-        n.addFirst();
-        List<String> d = new ArrayList<>(Arrays.asList(n.getMass()));
-        this.deps = d;
+    public Deps(List<String> deps) {
+        this.deps = deps;
     }
 
+    /**
+     * Метод для прямой сортировки.
+     *
+     * @return deps.
+     */
+    public List<String> sortForward() {
+        deps.sort(new Comparator<String>() {
+            @Override
+            public int compare(String o1, String o2) {
+                return o1.compareTo(o2);
+            }
+        });
+        return deps;
+    }
+
+    /**
+     * Метод для обратной сортировки.
+     *
+     * @return deps.
+     */
+    public List<String> sortBack() {
+        deps.sort(new Comparator<String>() {
+            @Override
+            public int compare(String o1, String o2) {
+                return -o1.compareTo(o2);
+            }
+        });
+        return deps;
+    }
+
+    public static void main(String[] args) {
+        Deps d = new Deps(new ArrayList<>(Arrays.asList("K1\\SK1", "K1\\SK2", "K1\\SK1\\SSK1",
+                "K1\\SK1\\SSK2", "K2", "K2\\SK1\\SSK1", "K2\\SK1\\SSK2", "K1", "K2\\SK1")));
+        System.out.println(d.sortForward());
+        System.out.println(d.sortBack());
+    }
 }
