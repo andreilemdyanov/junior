bb406049c5e549124ee3efa6c293cd61181a4a35
andreilemdyanov <lemdyanov5@mail.ru>
2017-06-17 00:49:13 +0400

    task 156

---
+++ chapter_005/src/main/java/pro/ru/job4j/generic/SimpleArray.java
@@ -0,0 +1,145 @@
+package pro.ru.job4j.generic;
+
+import java.util.Arrays;
+
+/**
+ * Class SimpleArray.
+ *
+ * @author Andrey Lemdyanov {lemdyanov5@mail.ru)
+ * @version $Id$
+ * @since 16.06.2017
+ */
+public class SimpleArray<T> {
+    /**
+     * Поле массив объектов.
+     */
+    private Object[] objects;
+    /**
+     * Поле индекс.
+     */
+    private int index = 0;
+
+    /**
+     * Геттер индекса.
+     *
+     * @return индекс.
+     */
+    public int getIndex() {
+        return index;
+    }
+
+    /**
+     * Сеттер индекса
+     *
+     * @param index новый индекс.
+     */
+    public void setIndex(int index) {
+        this.index = index;
+    }
+
+    /**
+     * Конструктор.
+     *
+     * @param size размер массива.
+     */
+    public SimpleArray(int size) {
+        this.objects = new Object[size];
+    }
+
+    /**
+     * Метод печатает переданное значение.
+     *
+     * @param value значение.
+     * @param <K>   тип.
+     * @return значение.
+     */
+    public <K> K print(K value) {
+        System.out.println(value);
+        return value;
+    }
+
+    /**
+     * Добавление.
+     *
+     * @param value элемент.
+     */
+    public void add(T value) {
+        this.objects[index++] = value;
+    }
+
+    /**
+     * Геттер элемента.
+     *
+     * @param position номер ячейки.
+     * @return элемент.
+     */
+    public T get(int position) {
+        return (T) this.objects[position];
+    }
+
+    /**
+     * Обновление элемента.
+     *
+     * @param position позиция в массиве.
+     * @param value    новое значение.
+     */
+    public void update(int position, T value) {
+        this.objects[position] = value;
+    }
+
+    /**
+     * Удаление элемента.
+     *
+     * @param value значение.
+     */
+    public void delete(T value) {
+        int pos = 0;
+        for (Object o : this.objects) {
+            if (o.equals(value)) {
+                Object[] temp = new Object[this.getIndex() - 1];
+                System.arraycopy(this.objects, 0, temp, 0, pos);
+                System.arraycopy(this.objects, pos + 1, temp, pos, this.objects.length - pos - 1);
+                this.objects = temp;
+                this.setIndex(index - 1);
+                break;
+            }
+            pos++;
+        }
+    }
+
+    /**
+     * Переопределение toString.
+     *
+     * @return строка.
+     */
+    @Override
+    public String toString() {
+        return "SimpleArray{" +
+                "objects=" + Arrays.toString(objects) +
+                ", index=" + index +
+                '}';
+    }
+
+    public static void main(String[] args) {
+        SimpleArray<String> s = new SimpleArray<String>(4);
+        s.add("a");
+        s.add("b");
+        s.add("c");
+        s.add("d");
+        System.out.println(s);
+        System.out.println(s.get(2));
+        s.update(2, "r");
+        System.out.println(s);
+        s.delete("r");
+        System.out.println(s);
+        s.print("d");
+        s.delete("a");
+        System.out.println(s);
+        s.delete("d");
+        System.out.println(s);
+        s.delete("b");
+        System.out.println(s);
+
+
+    }
+}
