b30d284d5b2a8b3dfa5d7b49474a1f494f64f1ed
andreilemdyanov <lemdyanov5@mail.ru>
2017-06-18 00:47:29 +0400

    task 152

--- chapter_005/src/main/java/pro/ru/job4j/iterator/IteratorInteger.java
+++ chapter_005/src/main/java/pro/ru/job4j/iterator/IteratorInteger.java
@@ -3,6 +3,7 @@ package pro.ru.job4j.iterator;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Iterator;
+import java.util.List;
 
 /**
  * Class IteratorInteger.
@@ -13,57 +14,55 @@ import java.util.Iterator;
  */
 public class IteratorInteger implements IteratorIter {
 
+    private Iterator<Integer> result;
 
-    private Iterator[] ar;
+    private Iterator<Iterator<Integer>> ar;
 
     private int position = 0;
 
-    public IteratorInteger(Iterator[] ar) {
+    public IteratorInteger(Iterator<Iterator<Integer>> ar) {
         this.ar = ar;
-    }
-
-    public void check() {
-        if (!ar[position].hasNext()) {
-            position++;
-        }
     }
 
     @Override
     public Object next() {
-        check();
-        return ar[position].next();
+        return ar.next();
     }
 
     @Override
     public boolean hasNext() {
-        if (position == ar.length) {
-            return false;
-        }
-        check();
-        if (position == ar.length) {
-            return false;
-        }
-        return ar[position].hasNext();
+
+        return ar.hasNext();
     }
+
 
     @Override
     public Iterator convert(Iterator it) {
-
-        return null;
+        List<Integer> list = new ArrayList<>();
+        IteratorInteger t = new IteratorInteger(it);
+        while(t.hasNext()) {
+            Iterator current =(Iterator) t.next();
+            while(current.hasNext()) {
+                list.add((Integer)(current.next()));
+            }
+        }
+        result = new ArrayList<Integer>(list).iterator();
+        return result;
     }
+
 
 
     public static void main(String[] args) {
         Iterator<Integer> it1 = new ArrayList<Integer>(Arrays.asList(1, 2, 3, 4, 5, 6, 7)).iterator();
         Iterator<Integer> it2 = new ArrayList<Integer>(Arrays.asList(11, 22, 33, 44, 55, 66, 77)).iterator();
         Iterator<Integer> it3 = new ArrayList<Integer>(Arrays.asList(10, 20, 30, 40, 50, 60, 70)).iterator();
-        Iterator[] mas = new Iterator[]{it1, it2, it3};
-        IteratorInteger iter = new IteratorInteger(mas);
-        while (iter.hasNext()) {
-            System.out.println(iter.next());
+        Iterator<Iterator<Integer>> it = Arrays.asList(it1, it2, it3).iterator();
+        IteratorInteger a = new IteratorInteger(it);
+        a.convert(it);
+        while(a.result.hasNext()) {
+            System.out.println(a.result.next());
         }
-        System.out.println(iter.hasNext());
-        System.out.println(iter.hasNext());
+
 
     }
 }
