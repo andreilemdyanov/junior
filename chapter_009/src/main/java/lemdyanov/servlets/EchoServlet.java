bf3878b81a203f5f3ac78384d31402a9ee4a414d
andreilemdyanov <lemdyanov5@mail.ru>
2018-04-06 00:24:46 +0400

    Парсер вакансий на sql.ru [#1731]

---
+++ chapter_009/src/main/java/lemdyanov/servlets/EchoServlet.java
@@ -0,0 +1,30 @@
+package lemdyanov.servlets;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+/**
+ * Class EchoServlet.
+ *
+ * @author Andrey Lemdyanov {lemdyanov5@mail.ru)
+ * @version $Id$
+ * @since 06.04.2018
+ */
+public class EchoServlet extends HttpServlet {
+    private static final Logger LOG = LoggerFactory.getLogger(EchoServlet.class);
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.setContentType("text/html");
+        PrintWriter writer = new PrintWriter(resp.getOutputStream());
+        writer.append("Hello World");
+        writer.flush();
+    }
+}
