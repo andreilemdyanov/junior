2924c3f560e59f2ec5d1fd64c5fe85d389b1004f
andreilemdyanov <lemdyanov5@mail.ru>
2018-08-21 00:38:35 +0400

    Лямбды и шаблон wrapper. [#49295]

--- chapter_011/todoList/src/main/java/dao/ItemDao.java
+++ chapter_011/todoList/src/main/java/dao/ItemDao.java
@@ -3,45 +3,51 @@ package dao;
 import models.Item;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
 import org.hibernate.cfg.Configuration;
 
 import java.sql.Timestamp;
 import java.util.List;
+import java.util.function.Function;
 
 /**
  * Class ItemDao.
  *
  * @author Andrey Lemdyanov {lemdyanov5@mail.ru)
  * @version $Id$
- * @since 16.08.2018
+ * @since 21.08.2018
  */
 public class ItemDao {
-    public List<Item> getAllItems() {
+    private <T> T tx(final Function<Session, T> command) {
         SessionFactory factory = new Configuration()
                 .configure()
                 .buildSessionFactory();
-        Session session = factory.openSession();
-        session.getTransaction().begin();
-        List<Item> items = session.createQuery("from Item").list();
-        session.getTransaction().commit();
-        session.close();
-        factory.close();
-        return items;
+        final Session session = factory.openSession();
+        final Transaction tx = session.beginTransaction();
+        try {
+            return command.apply(session);
+        } catch (final Exception e) {
+            session.getTransaction().rollback();
+            throw e;
+        } finally {
+            tx.commit();
+            session.close();
+        }
+    }
+
+    public List<Item> getAllItems() {
+        return this.tx(session -> session.createQuery("from Item").list());
     }
 
     public void addItem(String description) {
-        SessionFactory factory = new Configuration()
-                .configure()
-                .buildSessionFactory();
-        Session session = factory.openSession();
-        session.beginTransaction();
-        Item item = Item.builder()
-                .created(new Timestamp(System.currentTimeMillis()))
-                .description(description)
-                .build();
-        session.save(item);
-        session.getTransaction().commit();
-        session.close();
-        factory.close();
+        this.tx(session -> {
+            Item item = Item.builder()
+                    .created(new Timestamp(System.currentTimeMillis()))
+                    .description(description)
+                    .build();
+            session.save(item);
+            return session;
+        });
+
     }
 }
